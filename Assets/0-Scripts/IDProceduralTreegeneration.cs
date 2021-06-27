74e93bf7e794a89d42999d7e6f46eef02751d5f0
SinDeSiecle <yakin_murat@yahoo.com>
2021-06-27 18:38:36 +0300

    ecs

--- Assets/0-Scripts/IDProceduralTreegeneration.cs
+++ Assets/0-Scripts/IDProceduralTreegeneration.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+// using Unity.Rendering;
 
 public class IDProceduralTreegeneration : MonoBehaviour {
     public GameObject treePrefab;
     public int amount = 1000;
 
+    public Mesh prismMesh;
+    public Material prismMaterial;
+
     private float startTime, endTime;
     private bool isGenerationStarted;
+    public EntityManager _entityManager;
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.G) && !isGenerationStarted) {
@@ -46,35 +54,9 @@ public class IDProceduralTreegeneration : MonoBehaviour {
     private void GenerateTreesFast() {
         Vector3[] origins = new Vector3[amount];
         
-        for (int i=0; i<amount; i++) {
-            origins[i] = transform.TransformPoint(new Vector3( UnityEngine.Random.Range(-250, 250), 0, UnityEngine.Random.Range(-250,250)));
-        }
-        RaycastHit[] hits = PerformMultiRaycast(origins);
-        for (int i=0; i<hits.Length; i++) {
-            if (hits[i].collider.name != "NoFoliageCollider") {
-                Quaternion rot = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
-                float scale = UnityEngine.Random.Range(0.6f, 1.4f);
-                Vector3 locScale = Vector3.one * scale;
-                GameObject obj = Instantiate(treePrefab, hits[i].point, rot, transform);
-                obj.transform.localScale = locScale;
-            }
-        }
-
-        // Vector3[] randPoses = new Vector3[amount];
-        // NativeArray<Vector3> res = new NativeArray<Vector3>(amount, Allocator.TempJob);
-
-        // var randJob = new DefineRandomPosJob() {
-        //     randomMin = -250,
-        //     randomMax = 250,
-        //     results = res
-        // };
-        // JobHandle randJobHandle = randJob.Schedule(amount, 64);
-        // randJobHandle.Complete();
-
         // for (int i=0; i<amount; i++) {
-        //     origins[i] = transform.TransformPoint(res[i]);
+        //     origins[i] = transform.TransformPoint(new Vector3( UnityEngine.Random.Range(-250, 250), 0, UnityEngine.Random.Range(-250,250)));
         // }
-
         // RaycastHit[] hits = PerformMultiRaycast(origins);
         // for (int i=0; i<hits.Length; i++) {
         //     if (hits[i].collider.name != "NoFoliageCollider") {
@@ -86,7 +68,52 @@ public class IDProceduralTreegeneration : MonoBehaviour {
         //     }
         // }
 
-        // res.Dispose();
+
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(treePrefab, settings);
+
+        
+
+
+        Vector3[] randPoses = new Vector3[amount];
+        NativeArray<Vector3> res = new NativeArray<Vector3>(amount, Allocator.TempJob);
+
+        var randJob = new DefineRandomPosJob() {
+            randomMin = -250,
+            randomMax = 250,
+            rand = new Unity.Mathematics.Random(1),
+            results = res
+        };
+        JobHandle randJobHandle = randJob.Schedule(amount, 64);
+        randJobHandle.Complete();
+
+        for (int i=0; i<amount; i++) {
+            origins[i] = transform.TransformPoint(res[i]);
+        }
+
+        RaycastHit[] hits = PerformMultiRaycast(origins);
+        for (int i=0; i<hits.Length; i++) {
+            if (!string.IsNullOrEmpty(hits[i].collider.name)) {
+                if (hits[i].collider.name != "NoFoliageCollider") {
+                    Quaternion rot = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
+                    float scale = UnityEngine.Random.Range(0.6f, 1.4f);
+                    Vector3 locScale = Vector3.one * scale;
+                    // GameObject obj = Instantiate(treePrefab, hits[i].point, rot, transform);
+                    // obj.transform.localScale = locScale;
+
+                    var entityInstance = _entityManager.Instantiate(entity);
+                    float3 pos = hits[i].point;
+                    _entityManager.SetComponentData(entityInstance, new Translation { Value = pos });
+                    // _entityManager.SetSharedComponentData(entityInstance, new RenderMesh { mesh = prismMesh, material = prismMaterial });
+                }
+            }
+        }
+
+        res.Dispose();
+
+
+       
     }
 
     private Vector3 GetSurfaceWorldPosition(Vector3 aWorldPosition) {
@@ -124,12 +151,13 @@ public class IDProceduralTreegeneration : MonoBehaviour {
         return batchedHits;
     }
 
+    [BurstCompile]
     private struct DefineRandomPosJob : IJobParallelFor {
         public float randomMin, randomMax;
+        public Unity.Mathematics.Random rand;
         public NativeArray<Vector3> results;
         
         public void Execute(int index) {
-            var rand = new Unity.Mathematics.Random(1);
             results[index] = new Vector3(rand.NextFloat(randomMin, randomMax), 0, rand.NextFloat(randomMin, randomMax));
 
         }
